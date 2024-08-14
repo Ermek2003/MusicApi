72c66649ab177a71d7d5dc1847d2f2db591d2cb1
Ermek <etaldybaev1975@gmail.com>
2024-08-14 10:30:05 +0600

    Add migration Init

---
+++ WebApi/Extensions/EntityFrameworkExtension.cs
@@ -0,0 +1,13 @@
+﻿using DAL.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Extensions;
+
+public static class EntityFrameworkExtension
+{
+    public static void AddEntityFramework(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("Default")));
+    }
+}
